de50e1dbf2d876c4877713021844c03e9a246708
Yang <35453890+longrongyang@users.noreply.github.com>
2020-08-03 09:43:47 +0800

    Update noisy_labels_SN_cityscapes.py

--- noisy_labels_SN_cityscapes.py
+++ noisy_labels_SN_cityscapes.py
@@ -13,7 +13,8 @@ r = 0.2
 
 count = 0
 
-a = unjson('')
+p = ''
+a = unjson(p)
 for i in range(len(a['annotations'])):
     if np.random.random() < r:
         id = np.array([24, 25, 26, 27, 28, 31, 32, 33])
