5a3943e8cd969714b412b83df2e2fa10883cbee3
Yang <35453890+longrongyang@users.noreply.github.com>
2020-08-03 10:26:14 +0800

    Update noisy_labels_AN_VOC.py

--- noisy_labels_AN_VOC.py
+++ noisy_labels_AN_VOC.py
@@ -12,46 +12,36 @@ def unjson(file):
 r = 0.2
 
 count = 0
-c = 0
 
 p_a = ''
 p_g = ''
 a = unjson(p_a)
 for i in range(len(a['annotations'])):
-    if a['annotations'][i]['category_id'] == 3:
-        if np.random.random() < r:
+    if np.random.random() < r:
+        if a['annotations'][i]['category_id'] == 3:
             a['annotations'][i]['category_id'] = 1
             count += 1
-    elif a['annotations'][i]['category_id'] == 11:
-        if np.random.random() < r:
+        elif a['annotations'][i]['category_id'] == 11:
             a['annotations'][i]['category_id'] = 9
             count += 1
-    elif a['annotations'][i]['category_id'] == 6:
-        if np.random.random() < r:
+        elif a['annotations'][i]['category_id'] == 6:
             a['annotations'][i]['category_id'] = 7
             count += 1
-    elif a['annotations'][i]['category_id'] == 17:
-        if np.random.random() < r:
+        elif a['annotations'][i]['category_id'] == 17:
             a['annotations'][i]['category_id'] = 13
             count += 1
-    elif a['annotations'][i]['category_id'] == 2:
-        if np.random.random() < r:
+        elif a['annotations'][i]['category_id'] == 2:
             a['annotations'][i]['category_id'] = 14
             count += 1
-    elif a['annotations'][i]['category_id'] == 14:
-        if np.random.random() < r:
+        elif a['annotations'][i]['category_id'] == 14:
             a['annotations'][i]['category_id'] = 2
             count += 1
-    elif a['annotations'][i]['category_id'] == 8:
-        if np.random.random() < r:
+        elif a['annotations'][i]['category_id'] == 8:
             a['annotations'][i]['category_id'] = 12
             count += 1
-    elif a['annotations'][i]['category_id'] == 12:
-        if np.random.random() < r:
+        elif a['annotations'][i]['category_id'] == 12:
             a['annotations'][i]['category_id'] = 8
             count += 1
-    else:
-        c = c + 1
 with open(p_g, 'w') as file:
     json.dump(a, file)
 
