3e76120cb0dfafb14025cf10692a686b5e1eaadc
Yang <35453890+longrongyang@users.noreply.github.com>
2020-08-03 10:27:01 +0800

    Update noisy_labels_AN_VOC.py

--- noisy_labels_AN_VOC.py
+++ noisy_labels_AN_VOC.py
@@ -20,28 +20,21 @@ for i in range(len(a['annotations'])):
     if np.random.random() < r:
         if a['annotations'][i]['category_id'] == 3:
             a['annotations'][i]['category_id'] = 1
-            count += 1
         elif a['annotations'][i]['category_id'] == 11:
             a['annotations'][i]['category_id'] = 9
-            count += 1
         elif a['annotations'][i]['category_id'] == 6:
             a['annotations'][i]['category_id'] = 7
-            count += 1
         elif a['annotations'][i]['category_id'] == 17:
             a['annotations'][i]['category_id'] = 13
-            count += 1
         elif a['annotations'][i]['category_id'] == 2:
             a['annotations'][i]['category_id'] = 14
-            count += 1
         elif a['annotations'][i]['category_id'] == 14:
             a['annotations'][i]['category_id'] = 2
-            count += 1
         elif a['annotations'][i]['category_id'] == 8:
             a['annotations'][i]['category_id'] = 12
-            count += 1
         elif a['annotations'][i]['category_id'] == 12:
             a['annotations'][i]['category_id'] = 8
-            count += 1
+        count += 1
 with open(p_g, 'w') as file:
     json.dump(a, file)
 
