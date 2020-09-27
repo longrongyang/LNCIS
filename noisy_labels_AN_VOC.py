8328f6d0ce8853641d3a5d8a5ae7c713eb8cd375
Yang <35453890+longrongyang@users.noreply.github.com>
2020-09-27 11:08:49 +0800

    Update noisy_labels_AN_VOC.py

--- noisy_labels_AN_VOC.py
+++ noisy_labels_AN_VOC.py
@@ -18,20 +18,28 @@ p_g = ''
 a = unjson(p_a)
 for i in range(len(a['annotations'])):
     if np.random.random() < r:
+        # bird -> aeroplane
         if a['annotations'][i]['category_id'] == 3:
             a['annotations'][i]['category_id'] = 1
+        # diningtable -> chair
         elif a['annotations'][i]['category_id'] == 11:
             a['annotations'][i]['category_id'] = 9
+        # bus -> car
         elif a['annotations'][i]['category_id'] == 6:
             a['annotations'][i]['category_id'] = 7
+        # sheep -> horse
         elif a['annotations'][i]['category_id'] == 17:
             a['annotations'][i]['category_id'] = 13
+        # bicycle -> motorbike
         elif a['annotations'][i]['category_id'] == 2:
             a['annotations'][i]['category_id'] = 14
+        # motorbike -> bicycle
         elif a['annotations'][i]['category_id'] == 14:
             a['annotations'][i]['category_id'] = 2
+        # cat -> dog
         elif a['annotations'][i]['category_id'] == 8:
             a['annotations'][i]['category_id'] = 12
+        # dog -> cat
         elif a['annotations'][i]['category_id'] == 12:
             a['annotations'][i]['category_id'] = 8
         count += 1
